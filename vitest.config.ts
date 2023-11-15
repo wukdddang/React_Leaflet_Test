ea45d5242946a580ae03c91e591f13a6029290e3
wukdddang <dnr8874@naver.com>
2023-11-15 18:26:07 +0900

    Test: vitest, react testing library 관련 라이브러리 설정 추가

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+// vitest.config.ts
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    setupFiles: ["./src/setupTests.ts"],
+    environment: "jsdom",
+  },
+});
