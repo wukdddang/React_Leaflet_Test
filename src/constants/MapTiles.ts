31c292ac33dfbe823aa394d59ca65c26ddccd92a
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-11-17 00:49:14 +0900

    Feat: MapTiles.ts 타입 파일 추가

---
+++ src/constants/MapTiles.ts
@@ -0,0 +1,14 @@
+export const MAP_TILES = {
+  google_satellite: {
+    url: 'http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+    options: {
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    },
+  },
+  leaflet_osm: {
+    url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    options: {},
+  },
+} as const;
+
+export type KINDOF_MAP_TILES = keyof typeof MAP_TILES;
