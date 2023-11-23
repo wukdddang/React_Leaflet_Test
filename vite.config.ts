cb49709f50c71e67089bf6e33eef9a0cda94240b
wukdddang <dnr8874@naver.com>
2023-11-23 17:53:37 +0900

    Chore: 라이브러리 설치 + 환경설정 변경 - cypress 설치 - vitest-coverage 스크립트 추가 - alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
   },
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 // vitest.config.ts
+import path from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
@@ -6,5 +7,11 @@ export default defineConfig({
     globals: true,
     setupFiles: ["./src/setupTests.ts"],
     environment: "jsdom",
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
   },
 });
